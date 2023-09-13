8ba25940396180efc50de54fb7445fba2d822fc3
JeysonFlores <jeyson.flores@hotmail.com>
2023-09-13 01:14:07 -0700

    Added main app file

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import WelcomeScreen from "./screens/WelcomeScreen.svelte";
+  import GraphScreen from "./screens/GraphScreen.svelte";
+
+  let currentVehicle;
+
+  const setVehicleId = async (vehicleId) => {
+    currentVehicle = vehicleId;
+  };
+</script>
+
+
+
+
+{#if currentVehicle}
+  <GraphScreen vehicleId={currentVehicle}/>
+{:else}
+  <WelcomeScreen action={setVehicleId}/>
+{/if}
